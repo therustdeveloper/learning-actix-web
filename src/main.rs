3163fec638f57db292d35a3f478946837974a974
william <william.munozr@gmail.com>
2024-05-12 09:46:30 -0500

    structuring the project

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 
-
-async fn index() -> impl Responder {
-    "App Hello World!"
-}
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+mod views;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .service(
-                // prefixes all resources and routes attached to it...
-                web::scope("/app")
-                    .route("/index.html", web::get().to(index))
-            )
-            .route("/hey", web::get().to(manual_hello))
+        let app = App::new().configure(views::views_factory);
+        return app;
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
