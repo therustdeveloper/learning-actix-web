8e6654c93039286418a775ff5cc58e912b5c0be5
william <william.munozr@gmail.com>
2024-05-13 00:59:25 -0500

    structuring the to_do application as a module

--- src/views/to_do/mod.rs
+++ src/views/to_do/mod.rs
@@ -2,5 +2,5 @@ mod create;
 use actix_web::web::{post, scope, ServiceConfig};
 
 pub fn to_do_views_factory(app: &mut ServiceConfig) {
-    app.service(scope("v1/item").route("create", post().to(create::create)));
+    app.service(scope("v1/item").route("create/{title}", post().to(create::create)));
 }
