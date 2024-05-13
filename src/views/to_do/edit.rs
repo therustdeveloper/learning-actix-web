33bd14d421e5f0c50fecb6e902ddd05665854b90
William Munoz <william@rustdeveloper.io>
2024-05-13 13:37:51 -0500

    Add intercepting headers with a custom middleware

--- src/views/to_do/edit.rs
+++ src/views/to_do/edit.rs
@@ -1,4 +1,5 @@
 use crate::json_serialization::{to_do_item::ToDoItem, to_do_items::ToDoItems};
+use crate::jwt::JwToken;
 use crate::processes::process_input;
 use crate::state::read_file;
 use crate::to_do::{enums::TaskStatus, to_do_factory};
@@ -6,7 +7,8 @@ use actix_web::{web, HttpResponse};
 use serde_json::value::Value;
 use serde_json::Map;
 
-pub async fn edit(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
+pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JwToken) -> HttpResponse {
+    println!("here is the message in the token: {}", token.message);
     let state: Map<String, Value> = read_file("./state.json");
     let status: TaskStatus;
     match &state.get(&to_do_item.title) {
