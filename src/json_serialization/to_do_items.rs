e2584a0aa94b48309636720ec304a8466a75d98a
William Munoz <william@rustdeveloper.io>
2024-05-13 12:29:19 -0500

    Decoupling views from code

--- src/json_serialization/to_do_items.rs
+++ src/json_serialization/to_do_items.rs
@@ -1,6 +1,12 @@
 use crate::to_do::structs::base::Base;
 use crate::to_do::ItemTypes;
 use serde::Serialize;
+use std::vec::Vec;
+use serde_json::value::Value;
+use serde_json::Map;
+use actix_web::{body::BoxBody, http::header::ContentType, HttpRequest, HttpResponse, Responder};
+use crate::state::read_file;
+use crate::to_do::{to_do_factory, enums::TaskStatus};
 
 #[derive(Serialize)]
 pub struct ToDoItems {
@@ -31,5 +37,29 @@ impl ToDoItems {
             pending_item_count: pending_count,
             done_items: done_array_buffer,
         };
+    }
+
+    pub fn get_state() -> ToDoItems {
+        let state: Map<String, Value> = read_file("./state.json");
+        let mut array_buffer = Vec::new();
+
+        for (key, value) in state {
+            let status = TaskStatus::from_string(value.as_str().unwrap().to_string());
+            let item = to_do_factory(&key, status);
+            array_buffer.push(item);
+        }
+
+        return ToDoItems::new(array_buffer)
+    }
+}
+
+impl Responder for ToDoItems {
+    type Body = BoxBody;
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
     }
 }
--- src/views/to_do/get.rs
+++ src/views/to_do/get.rs
@@ -1,19 +1,6 @@
 use crate::json_serialization::to_do_items::ToDoItems;
-use crate::state::read_file;
-use crate::to_do::{enums::TaskStatus, to_do_factory, ItemTypes};
-use actix_web::{web, Responder};
-use serde_json::value::Value;
-use serde_json::Map;
+use actix_web::Responder;
 
 pub async fn get() -> impl Responder {
-    let state: Map<String, Value> = read_file("./state.json");
-    let mut array_buffer = Vec::new();
-
-    for (key, value) in state {
-        let status = TaskStatus::from_string(value.as_str().unwrap().to_string());
-        let item: ItemTypes = to_do_factory(&key, status);
-        array_buffer.push(item);
-    }
-    let return_package: ToDoItems = ToDoItems::new(array_buffer);
-    return web::Json(return_package);
+    return ToDoItems::get_state();
 }
