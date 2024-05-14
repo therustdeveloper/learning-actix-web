028d6d4f744f9d66efb7084f2177708761cdba17
william <william.munozr@gmail.com>
2024-05-13 22:28:56 -0500

    formatting code

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -1,17 +1,12 @@
-use actix_web::HttpResponse;
-use super::content_loader::read_file;
 use super::content_loader::add_component;
-
+use super::content_loader::read_file;
+use actix_web::HttpResponse;
 
 pub async fn items() -> HttpResponse {
-    let mut html_data = read_file(
-        "./templates/main.html");
-    let javascript_data: String = read_file(
-        "./javascript/main.js");
-    let css_data: String = read_file(
-        "./css/main.css");
-    let base_css_data: String = read_file(
-        "./css/base.css");
+    let mut html_data = read_file("./templates/main.html");
+    let javascript_data: String = read_file("./javascript/main.js");
+    let css_data: String = read_file("./css/main.css");
+    let base_css_data: String = read_file("./css/base.css");
 
     html_data = html_data.replace("{{JAVASCRIPT}}", &javascript_data);
     html_data = html_data.replace("{{CSS}}", &css_data);
@@ -22,5 +17,3 @@ pub async fn items() -> HttpResponse {
         .content_type("text/html; charset=utf-8")
         .body(html_data)
 }
-
-
